461100abb85be64aacd7c6ab5fef7bc454698728
alek <alek.rutkowski@gmail.com>
2022-11-21 16:40:29 +0100

    Added syntax highlighting

--- app.R
+++ app.R
@@ -5,6 +5,7 @@ library(shiny)
 library(shinybusy)
 library(rclipboard)
 library(xml2)
+library(prismjs)
 
 # Helpers
 
@@ -113,10 +114,14 @@ urlStructure <- memoise::memoise(function(ds_code)
         DimensionList} %>% 
     sapply(function(x) attr(x$ConceptIdentity$Ref,'id')))
 
+prism_style <-
+  "code[class*=language-],pre[class*=language-]{color:#000;background:0 0;text-shadow:0 1px #fff;font-family:Consolas,Monaco,'Andale Mono','Ubuntu Mono',monospace;font-size:1em;text-align:left;white-space:pre;word-spacing:normal;word-break:normal;word-wrap:normal;line-height:1.5;-moz-tab-size:4;-o-tab-size:4;tab-size:4;-webkit-hyphens:none;-moz-hyphens:none;-ms-hyphens:none;hyphens:none}code[class*=language-] ::-moz-selection,code[class*=language-]::-moz-selection,pre[class*=language-] ::-moz-selection,pre[class*=language-]::-moz-selection{text-shadow:none;background:#b3d4fc}code[class*=language-] ::selection,code[class*=language-]::selection,pre[class*=language-] ::selection,pre[class*=language-]::selection{text-shadow:none;background:#b3d4fc}@media print{code[class*=language-],pre[class*=language-]{text-shadow:none}}pre[class*=language-]{padding:1em;margin:.5em 0;overflow:auto}:not(pre)>code[class*=language-],pre[class*=language-]{background:#f5f2f0}:not(pre)>code[class*=language-]{padding:.1em;border-radius:.3em;white-space:normal}.token.cdata,.token.comment,.token.doctype,.token.prolog{color:#708090}.token.punctuation{color:#999}.token.namespace{opacity:.7}.token.boolean,.token.constant,.token.deleted,.token.number,.token.property,.token.symbol,.token.tag{color:#905}.token.attr-name,.token.builtin,.token.char,.token.inserted,.token.selector,.token.string{color:#690}.language-css .token.string,.style .token.string,.token.entity,.token.operator,.token.url{color:#9a6e3a;background:hsla(0,0%,100%,.5)}.token.atrule,.token.attr-value,.token.keyword{color:#07a}.token.class-name,.token.function{color:#dd4a68}.token.important,.token.regex,.token.variable{color:#e90}.token.bold,.token.important{font-weight:700}.token.italic{font-style:italic}.token.entity{cursor:help}"
+
 # Application
 shinyApp(
   ui = fluidPage(
-    tags$head(tags$style(HTML('* {font-weight: bold; font-family: monospace};'))),
+    tags$head(tags$style(HTML('* {font-weight: bold; font-family: monospace};',
+                              prism_style))),
     rclipboardSetup(),
     add_busy_spinner(spin="fading-circle", position='full-page',
                      height='100px', width='100px'),
@@ -146,7 +151,7 @@ shinyApp(
       column(6,
              uiOutput("clip"),
              br(),
-             verbatimTextOutput("value")
+             htmlOutput("value")
       ))),
   server = function(input, output) {
     metadata <- reactive(
@@ -281,12 +286,15 @@ shinyApp(
                  '.[\n',.,'] %>%\n',
                  unneededColsCode(md),
                  dcastCode(md),
-                 AltRcode())
+                 AltRcode()) %>% 
+          list(txt=.,
+               html=prismjs::prism_highlight_text(.,language='r') %>% 
+                 paste0('<pre class="shiny-text-output" style="background-color:white;">',.,'</pre>'))
       })
     output$value <-
-      reactive(Rcode())
+      reactive(Rcode()$html)
     output$clip <- renderUI({
-      rc <- Rcode()
+      rc <- Rcode()$txt
       if (!is.null(rc))
         rclipButton(
           inputId = "clipbtn",
